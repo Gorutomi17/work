3b46b453a53c36d5a14bcd36bd98bf78c24e4f83
zwcdemon <99344145+zwcdemon@users.noreply.github.com>
2022-06-01 13:12:46 +0700

    Update root.c

--- root.c
+++ root.c
@@ -1,32 +1,4 @@
-//
-// This exploit uses the pokemon exploit of the dirtycow vulnerability
-// as a base and automatically generates a new passwd line.
-// The user will be prompted for the new password when the binary is run.
-// The original /etc/passwd file is then backed up to /tmp/passwd.bak
-// and overwrites the root account with the generated line.
-// After running the exploit you should be able to login with the newly
-// created user.
-//
-// To use this exploit modify the user values according to your needs.
-//   The default is "daemons".
-//
-// Original exploit (dirtycow's ptrace_pokedata "pokemon" method):
-//   https://github.com/dirtycow/dirtycow.github.io/blob/master/pokemon.c
-//
-// Compile with:
-//   gcc -pthread dirty.c -o dirty -lcrypt
-//
-// Then run the newly create binary by either doing:
-//   "./dirty" or "./dirty my-new-password"
-//
-// Afterwards, you can either "su daemons" or "ssh daemons@..."
-//
-// DON'T FORGET TO RESTORE YOUR /etc/passwd AFTER RUNNING THE EXPLOIT!
-//   mv /tmp/passwd.bak /etc/passwd
-//
-// Exploit adopted by Christian "daemons" Mehlmauer
-// https://daemons.at
-//
+//recode dari dirty
 
 #include <fcntl.h>
 #include <pthread.h>
